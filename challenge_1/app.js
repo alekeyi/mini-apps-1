ce17b6fc6e102e83e0b87d1f3ee8d3cb9fcb80e7
alekeyi <aleksey.ioseliani@gmail.com>
2019-06-24 15:36:11 -0500

    Created elementary click functionality

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,11 @@
 console.log("Is app.js loading into HTML???");
 
+// set a variable to be toggled in order to determine whose turn it is
+let playerMark = true;
+
+document.getElementById("board").addEventListener("click", function(event) {
+    // event.target clicked element
+    if (event.target /*&& event.target.nodeName == "td"*/) {
+        console.log(event.target.id, " was clicked");
+    }
+})
