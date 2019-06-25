6798fbd48d171565b1dcf29341112eb053eadfc9
alekeyi <aleksey.ioseliani@gmail.com>
2019-06-25 11:59:31 -0500

    Creates package.json, creates basic server linked to static client, sets up elementary html, routes queries

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,7 @@
+// check if app.js is connected
+console.log("Here be app.js, yarr");
 
-
+// clear form after submission
+function clearForm() {
+    document.getElementById("text-area").value = '';
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 4000;
 
+// create static server which refers to client folder
+app.use(express.static('client'));
+// express middleware used to extract form-data sent by post
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+    console.log('Get hit on the main page!');
+    res.send('Howdy!');
+});
+
+app.post('/submit', (req, res) => {
+    console.log('Post submit on the main page');
+    res.send("YUP");
+})
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
 
