44c6acbabb3827beaf14d3392cd3396b3ed8e048
alekeyi <aleksey.ioseliani@gmail.com>
2019-06-25 16:20:22 -0500

    Manipulates requests

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -3,12 +3,23 @@ console.log("Here be app.js, yarr");
 
 // clear form after submission
 function clearForm() {
-  //   let form = document.getElementById("text-area").value;
+  // let form = document.getElementById("text-area").value;
   let form = document.getElementById("form");
-  console.log(form);
-  //   form = "";
-  //   document.getElementById("text-area").value = "";
+  // console.log(form);
+  // document.getElementById("text-area").value = "";
   form.submit();
   form.reset();
   return false;
+}
+
+// display stored sales reports
+function displaySalesReport() {
+    // initialize display variable with target div
+    let disp = document.getElementById("salesReps");
+    // toggle styling of target div between "none" and "block" display
+    if(disp.style.display === "none") {
+        disp.style.display = "block";
+    } else {
+        disp.style.display = "none";
+    }
 }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+let serverArray = [];
+
 // create static server which refers to client folder
 app.use(express.static("client"));
 // express middleware used to extract form-data sent by post, can use native instead of bodyParser
@@ -9,8 +11,6 @@ app.use(express.static("client"));
 app.use(express.urlencoded({ extended: true }));
 // can use native express.json() instead of bodyParser.json()
 app.use(express.json());
-// // use validator to check input and report errors before creation of input data
-// const { check, validationResult } = require("express-validator");
 
 // get request for main page, returns index.html due to express.static above
 app.get("/", (req, res) => {
@@ -18,37 +18,29 @@ app.get("/", (req, res) => {
   // res.send('Howdy!');
 });
 
-// post request to submit form data via submit button
-app.post(
-  "/submit",
-  /*[ //insert middleware
-        // form data must be at least 5 chars long
-        check('formData').isLength({ min: 5 })
-    ],*/ (
-    req,
-    res
-  ) => {
-    // res, req function
-    /*// finds any validation errors and wraps in object
-        const errors = validationResult(req);
-        // if error
-        if (!errors.isEmpty()) {
-            // returns a status code of 422: Unprocessable Entity and the errors in an array
-            return res.status(422).json({ errors: errors.array() });
-        }*/
+app.get("/submit", (req, res) => {
+    console.log("Thru the looking glass -- get /submit");
+    console.log("REQ.QUERY: ", req.query);
+    res.send(serverArray);
+});
 
-    console.log("Post submit on the main page");
-    console.log("\nREQUEST HEADERS: \n", req.headers);
-    // console.log("\nForm data in REQUEST BODY: \n", req.body);
-    // SalesReport.create({
-    //     formData: req.body.formData
-    // }).then(inputData => res.json(inputData));
-    // const formData = req.body.formData;
-    // console.log("formData: ", formData);
-    // res.send(formData);
-    res.json(req.body.formData);
-    console.log("\nreq.body.formData", req.body.formData);
-  }
-);
+// post request to submit form data via submit button
+app.post("/submit", (req, res) => {
+  // res, req function
+  console.log("Post submit on the main page");
+  // console.log("\nREQUEST HEADERS: \n", req.headers);
+  // console.log("\nForm data in REQUEST BODY: \n", req.body);
+  let trimmedData = req.body.formData.replace(/\s+/g, ' ').trim();
+  serverArray.push(trimmedData);
+//   let postData = document.getElementById("salesReps");
+//   postData.innerHTML = serverArray;
+  console.log("REQ.PARAMS: ", req.params);
+  console.log("REQ.PARAM(): ", req.param());
+//   console.log("REQ.QUERY: ", req.query);
+  // res.json(req.body.formData);
+  res.send(req.body.formData);
+  // res.render(req.body.formData);
+  console.log("\nreq.body.formData", req.body.formData);
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
