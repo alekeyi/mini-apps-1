cc3c9ece7ac688e8830f6eb736a56c0941fe4896
alekeyi <aleksey.ioseliani@gmail.com>
2019-06-26 15:03:18 -0500

    Establishes server, client, and their communication, introduces dynamic HTML rendering

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,39 @@
+// see if babel is working
+console.log("Hi darlin");
 
+// import React
+// import React from 'react'; // don't need this for some reason, because index.html calls react scripts
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkingOut: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      checkingOut: true
+    });
+  }
+
+  render() {
+    //     return (
+    //       <div id="cowboy" style={{ flex: 1 }}>
+    //         <h2>Howdy, fool</h2>
+    //         <br />
+    //         <img src="cowboy.jpg" alt="Italian Trulli" />
+    //       </div>
+    //     );
+    return (
+      <div>
+        <button onClick={this.handleClick}>Checkout</button>
+        {this.state.checkingOut ? <F1 /> : null}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("App"));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,17 @@
+// bring in express
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 
+// setup static
+app.use(express.static("public"));
 
+// setup routes
+// app.get("/", (req, res) => {
+//   res.send("Dodge this.");
+// });
+
+// setup listener
+app.listen(3000, () => {
+  console.log("`I'm 'listenieng'` - DaServer... `Port: 3000, babey`");
+});
