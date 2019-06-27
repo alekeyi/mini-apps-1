2922070dd4a985ab9f8fe77f3b1c080c61599468
alekeyi <aleksey.ioseliani@gmail.com>
2019-06-26 19:10:15 -0500

    Renders all forms

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -36,14 +36,35 @@ class F1 extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      // name: '',
+      // email: '',
+      // password: '',
       nextF2: false
     };
   }
 
-  handleClick() {
+  handleClick(event) {
+    event.preventDefault();
+    const F1data = new FormData(event.target);
+    console.log('F1data: ', F1data);
+    console.log("event.target ON SUBMIT: ", event.target);
+    console.log("event.target.name ON SUBMIT: ", event.target.name);
+    console.log("event.target.name.innerHTML ON SUBMIT: ", event.target.name.innerHTML);
+    console.log("event.target.email ON SUBMIT: ", event.target.email);
+    console.log("event.target.password ON SUBMIT: ", event.target.password);
     this.setState({
+      // name: event.target.name,
+      // email: event.target.email,
+      // password: event.target.password,
       nextF2: true
     });
+  }
+
+  handleChange() {
+    console.log("event.target.value: ", event.target.value);
+    // this.setState({
+    //   name: event.target.value
+    // })
   }
 
   render() {
@@ -52,7 +73,21 @@ class F1 extends React.Component {
     } else {
       return (
         <div>
-          <button onClick={() => this.setState({ nextF2: true })}>Next</button>
+          <form onSubmit={(event) => {this.handleClick(event)}}>
+          <label>
+            Name:<br/><br/>
+            <input type="text" id="name" name="name" name={this.state.name} onChange={this.handleChange}></input>
+          </label><br/><br/>
+          <label>
+            Email:<br/><br/>
+            <input type="email" id="email" name="email" email={this.state.email} onChange={this.handleChange}></input>
+          </label><br/><br/>
+          <label>
+            Password:<br/><br/>
+            <input type="password" id="password" name="password" password={this.state.password} onChange={this.handleChange}></input>
+          </label><br/><br/>
+          <input type="submit" value="Next" /*onSubmit={() => this.setState({ nextF2: true })}*//>
+          </form>
         </div>
       );
     }
@@ -67,7 +102,8 @@ class F2 extends React.Component {
     };
   }
 
-  handleClick() {
+  handleClick(event) {
+    event.preventDefault();
     this.setState({
       nextF3: true
     });
@@ -79,7 +115,29 @@ class F2 extends React.Component {
     } else {
       return (
         <div>
-          <button onClick={() => this.setState({ nextF3: true })}>Next</button>
+          <form onSubmit={(event) => {this.handleClick(event)}}>
+          <label>
+            Mailing Address:<br/><br/>
+            <input type="text" id="line1" name="line1" name={this.state.line1} onChange={this.handleChange}></input>
+          </label><br/><br/>
+          <label>
+            Address Line 2:<br/><br/>
+            <input type="text" id="line2" name="line2" name={this.state.line2} onChange={this.handleChange}></input>
+          </label><br/><br/>
+          <label>
+            City:<br/><br/>
+            <input type="text" id="city" name="city" name={this.state.city} onChange={this.handleChange}></input>
+          </label><br/><br/>
+          <label>
+            State:<br/><br/>
+            <input type="text" id="state" name="state" name={this.state.state} onChange={this.handleChange}></input>
+          </label><br/><br/>
+          <label>
+            Zip code:<br/><br/>
+            <input type="text" id="zipcode" name="zipcode" name={this.state.zipcode} onChange={this.handleChange}></input>
+          </label><br/><br/>
+          <input type="submit" value="Next"/>
+          </form>
         </div>
       );
     }
@@ -94,7 +152,8 @@ class F3 extends React.Component {
     };
   }
 
-  handleClick() {
+  handleClick(event) {
+    event.preventDefault();
     this.setState({
       purchase: true
     });
@@ -102,11 +161,56 @@ class F3 extends React.Component {
 
   render() {
     if (this.state.purchase) {
+      return <Purchase />;
+    } else {
+      return (
+        <div>
+          <form onSubmit={(event) => {this.handleClick(event)}}>
+          <label>
+            Credit Card #:<br/><br/>
+            <input type="text" id="cc" name="cc" name={this.state.cc} onChange={this.handleChange}></input>
+          </label><br/><br/>
+          <label>
+            Expiry Date:<br/><br/>
+            <input type="date" id="date" name="date" name={this.state.date} onChange={this.handleChange}></input>
+          </label><br/><br/>
+          <label>
+            CVV:<br/><br/>
+            <input type="password" id="cvv" name="cvv" name={this.state.cvv} onChange={this.handleChange}></input>
+          </label><br/><br/>
+          <label>
+            Billing Zip code:<br/><br/>
+            <input type="text" id="billing" name="billing" name={this.state.billing} onChange={this.handleChange}></input>
+          </label><br/><br/>
+          <input type="submit" value="Next"/>
+          </form>
+        </div>
+      );
+    }
+  }
+}
+
+class Purchase extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      finish: false
+    };
+  }
+
+  handleClick() {
+    this.setState({
+      finish: true
+    });
+  }
+
+  render() {
+    if (this.state.finish) {
       return <App />;
     } else {
       return (
         <div>
-          <button onClick={() => this.setState({ purchase: true })}>
+          <button onClick={() => this.setState({ finish: true })}>
             Purchase
           </button>
         </div>
